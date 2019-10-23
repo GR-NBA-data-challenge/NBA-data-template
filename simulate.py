eb23399f3ac457bc9405eff33d2417da86e9f20e
GR-NBA-data-challenge <55437901+GR-NBA-data-challenge@users.noreply.github.com>
2019-10-23 15:57:05 +0000

    Integrate with datapack (#1)

--- simulate.py
+++ simulate.py
@@ -1,29 +1,192 @@
 import os
 import datetime
+import argparse
+import requests
+import urllib.parse
+import sys
+import re
+import traceback
+import json
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--cutoff', help='A datestring in format YYYY-MM-DD. The end of the training daterange, and the start of the week to be predicted. All data after the cutoff is automatically removed to avoid look-ahead', default='2019-01-01')
+
+# Hidden argument to switch to the dev server
+parser.add_argument('--dev', help=argparse.SUPPRESS, action="store_true")
+
+# Hidden argument to change the path in which to look the user code.
+# It is relative to the current file.
+parser.add_argument('--userpath', help=argparse.SUPPRESS, default='src')
+
+# Hidden argument to write out the result to a file
+parser.add_argument('--resultpath', help=argparse.SUPPRESS, default=None)
+
+# Hidden argument to write out logs to a file
+parser.add_argument('--logpath', help=argparse.SUPPRESS, default=None)
+
+args = parser.parse_args()
+
+env = 'dev' if args.dev else 'prod'
+
+currdir = os.path.abspath(os.path.dirname(__file__))
+
+userpath = os.path.abspath(os.path.join(currdir, args.userpath))
+
+sys.path.append(userpath)
+
+if args.logpath is not None:
+    logfile = open(args.logpath, 'a')
+
+def checkStatus(response):
+    if response.status_code < 200 or response.status_code > 299:
+        raise Exception(f'Could not obtain season data for season {season}. Server responded with status code {response.status_code}')
+
+class NbaDataLoader:
+    def __init__(self):
+        pass
+
+    # Obtain the games of a season.
+    # Seasons are strings such as '2009' or '2010POST'
+    # The earliest available season is '2009'
+    def getSeason(self, season: str):
+        r = requests.get(f'https://{env}api.nbadatachallenge.com/data/seasons/{urllib.parse.quote(season)}')
+        checkStatus(r)
+        data = r.json()
+        result = []
+        for d in data:
+            dateTime = d['dateTime']
+            if dateTime < args.cutoff:
+                result.append(d)
+        return result
+    
+    # Obtain a single game data
+    # The gameId is a numerical game identifier.
+    # You can find the gameId from the results of getSeason
+    def getGame(self, gameId: int):
+        r = requests.get(f'https://{env}api.nbadatachallenge.com/data/games/{urllib.parse.quote(str(gameId))}')
+        checkStatus(r)
+        data = r.json()
+        result = []
+        for d in data:
+            dateTime = d['dateTime']
+            if dateTime < args.cutoff:
+                result.append(d)
+        return result
+    
+    # Obtain full player data about all the games in a season.
+    def getPlayers(self, season: str):
+        r = requests.get(f'https://{env}api.nbadatachallenge.com/data/gameplayersfull/{urllib.parse.quote(season)}')
+        checkStatus(r)
+        data = r.json()
+        result = []
+        for d in data:
+            dateTime = d['dateTime']
+            if dateTime < args.cutoff:
+                result.append(d)
+        return result
+
+def loadPredictions():
+    r = requests.get(f'https://{env}api.nbadatachallenge.com/data/predictions/{urllib.parse.quote(args.cutoff)}')
+    checkStatus(r)
+    return r.json()
 
 def log(msg):
-    print(f'{datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()} {msg}')
+    formatted = f'{datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()} {msg}'
+    print(formatted)
+    if args.logpath is not None:
+        logfile.write(formatted + '\n')
 
-def historical_data():
-    log('Requested historical data')
+def findByGameId(results, gameId):
+    for r in results:
+        if r['gameId'] == gameId:
+            return r
+    return None
 
-def current_season_matches():
-    log('Requested current season matches')
+def getField(doc, field):
+    if doc is None:
+        return 'None'
+    if field in doc:
+        return doc[field]
+    else:
+        return 'None'
+
+def computeSum(a, b):
+    if a is None or b is None:
+        return None
+    return a + b
+
+def computeDiff(a, b):
+    if a is None or b is None:
+        return None
+    return a - b
+
+def displayPredictionsAndResults(results, actual):
+    for game in actual:
+        gameId = game['gameId']
+        resultGame = findByGameId(results, gameId)
+        homeScore = getField(game, 'homeScore')
+        awayScore = getField(game, 'awayScore')
+        actualSum = computeSum(homeScore, awayScore)
+        actualDiff = computeDiff(homeScore, awayScore)
+        sumPredicted = getField(resultGame, 'sum')
+        awayPredicted = getField(resultGame, 'diff')
+        log(f'Game {gameId}. Actual results: home {homeScore} - away {awayScore}. Actual: sum {actualSum} - diff {actualDiff}. Predicted results: sum {sumPredicted} - diff {awayPredicted}')
+
+def sanitizeResult(results, predictions):
+    if len(results) != len(predictions):
+        raise Exception(f'User returned {len(results)} predictions, but expecting {len(predictions)} predictions')
+    sanitized = []
+    for result in results:
+        if not isinstance(result['gameId'], int):
+            raise Exception(f'gameId field in the prediction must be an int')
+        if not isinstance(result['sum'], int):
+            raise Exception(f'sum field in the prediction must be an int')
+        if not isinstance(result['diff'], int):
+            raise Exception(f'diff field in the prediction must be an int')
+        sanitized.append({
+            'gameId': result['gameId'],
+            'sum': result['sum'],
+            'diff': result['diff']
+        })
+    return sanitized
 
 def entry():
     try:
+        if not re.match('^\d\d\d\d-\d\d-\d\d$', args.cutoff):
+            log(f'--cutoff argument value is not valid. Expected a YYYY-MM-DD format')
+            return
+
+        log(f'Loading prediction matches starting from {args.cutoff}')
+        predictionsFull = loadPredictions()
+        predictions = []
+        for prediction in predictionsFull:
+            predictions.append({
+                'date': prediction['date'],
+                'homeTeam': prediction['homeTeam'],
+                'awayTeam': prediction['awayTeam'],
+                'gameId': prediction['gameId']
+            })
+
         log('Loading user defined predict module')
-        from src import main
+        import main
         log('User defined predict module loaded')
-        required_predictions = [
-            {'teamA': 'teamA', 'teamB': 'teamB'},
-            {'teamA': 'teamC', 'teamB': 'teamD'}
-        ]
+
+        dataLoader = NbaDataLoader()
+
         log('Starting call to user defined function')
-        result = main.predict(required_predictions, historical_data, current_season_matches, log)
+        result = main.predict(predictions, dataLoader, log)
         log('User defined function completed')
-        log(f'User returned: {result}')
-    except Exception as e:
-        log(f'{e}')
+        result = sanitizeResult(result, predictionsFull)
+        displayPredictionsAndResults(result, predictionsFull)
+
+        if args.resultpath is not None:
+            log('Writing result...')
+            resultfile = open(args.resultpath, 'w')
+            resultfile.write(json.dumps(result))
+            resultfile.close()
+
+    except Exception:
+        log(f'An error occurred: {traceback.format_exc()}')
 
 entry()
--- src/main.py
+++ src/main.py
@@ -1,31 +1,42 @@
 # This is the main function you are requested to define.
-# required_predictions: a list of matches that you should predict. Each match is a dictionary
-#     containing two keys: "teamA", the name of the first team, and "teamB", the name of the second team.
-# historical_data: call to obtain the full data of all previous years. This data item does not change over time.
-# current_season_matches: call to obtain the full data of all previous games already played in the current season.
-#     This does not overlap with historical_data. This is empty when predicting the first match
-#     of the current season. As the season progresses, this item will gradually include
-#     previous matches of the current season.
+# required_predictions: a list of matches that you should predict
+# data_loader: a NbaDataLoader class instance.
+#     Contains getSeason, getGame and getPlayers methods. Please refer to simulate.py for details about
+#     the provided methods.
 # log: you can call log('message') to log any information you want. These messages are automatically
 #     printed to console, and to the logfiles when running on the server side.
 #     All logs sent to this logger can be retrieved from our servers (up to a maximum size), so
 #     use this to debug issues when running your code in our simulations.
 # (return): You should return, for each match requested in required_predictions, two numbers:
-#     The difference between the scores: teamAscore - teamBscore
-#     And the sum of the scores: teamAscore + teamBscore
-#     The result format should be a list of {"teamA": "X", "teamB": "Y", "diff": 25, "sum": 163}
-def predict(required_predictions, historical_data, current_season_matches, log):
-    # This example code predicts that the first team always wins every match
-    hist = historical_data()
-    curr = current_season_matches()
+#     The difference between the scores: homeScore - awayScore
+#     And the sum of the scores: homeScore - awayScore
+def predict(required_predictions, data_loader, log):
+    # Load games data for the 2020 season.
+    # Seasons from 2009 onwards are available, including POST seasons, such as 2010POST
+    games2010 = data_loader.getSeason('2010')
+    log(f'Loaded {len(games2010)} 2010 games')
+
+    # Loading a season that is ahead of the cutoff training time returns no results.
+    # In this case, the default cutoff time is in 2019, so loading 2020 data returns no results.
+    # You can change the cutoff time by passing to simulate.py
+    #     --cutoff YYYY-MM-DD
+    games2020 = data_loader.getSeason('2020')
+    log(f'Loaded {len(games2020)} 2019 games')
+
+    # You can load an individual match's data
+    aGame = data_loader.getGame(games2010[100]['gameId'])
+    log(f'Game 100 in the 2010 database is {aGame}')
+
+    # You can also load the full players data for an entire season
+    full2010seasonPlayers = data_loader.getPlayers('2010')
+    log(f'Loaded {len(full2010seasonPlayers)} 2010 season player rows')
+
     result = []
     for match in required_predictions:
-        # In this example we always predict that teamA wins by 100 points
         result.append({
-            "teamA": match["teamA"],
-            "teamB": match["teamB"],
-            "diff": 100,
-            "sum": 300
+            'gameId': match['gameId'],
+            'sum': 999,
+            'diff': 1
         })
     log('finished')
     return result
