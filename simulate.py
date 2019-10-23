ba4c1c42f031de8c0bc69d4ed0d674a14b99b19f
GR-NBA-data-challenge <nba@gresearch.info>
2019-10-23 08:28:51 +0000

    Connect to new server style, argument parsing

--- simulate.py
+++ simulate.py
@@ -1,14 +1,55 @@
 import os
 import datetime
+import argparse
+import requests
+import urllib.parse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--dev', help='Connect to the dev servers instead of the production ones', action="store_true")
+
+# TODO overridable path where to find the user defined code
+
+args = parser.parse_args()
+
+env = 'dev' if args.dev else 'prod'
+
+class NbaDataLoader:
+    def __init__(self):
+        pass
+
+    # TODO Handle data cutoff to prevent lookahead
+
+    # Obtain the games of a season.
+    # Seasons are strings such as '2009' or '2010POST'
+    # The earliest available season is '2009'
+    def getSeason(self, season: str):
+        r = requests.get(f'https://{env}api.nbadatachallenge.com/data/seasons/{urllib.parse.quote(season)}')
+        self._checkStatus(r)
+        return r.json()
+    
+    # Obtain a single game data
+    # The gameId is a numerical game identifier.
+    # You can find the gameId from the results of getSeason
+    def getGame(self, gameId: int):
+        r = requests.get(f'https://{env}api.nbadatachallenge.com/data/games/{urllib.parse.quote(gameId)}')
+        self._checkStatus(r)
+        return r.json()
+    
+    # Obtain full player data about all the games in a season.
+    def getPlayers(self, season: str):
+        r = requests.get(f'https://{env}api.nbadatachallenge.com/data/gameplayersfull/{urllib.parse.quote(season)}')
+        self._checkStatus(r)
+        return r.json()
+
+    # ----- Private methods below -----
+
+    def _checkStatus(self, response):
+        if response.status_code < 200 or response.status_code > 299:
+            raise Exception(f'Could not obtain season data for season {season}. Server responded with status code {response.status_code}')
 
 def log(msg):
     print(f'{datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()} {msg}')
-
-def historical_data():
-    log('Requested historical data')
-
-def current_season_matches():
-    log('Requested current season matches')
 
 def entry():
     try:
@@ -19,8 +60,11 @@ def entry():
             {'teamA': 'teamA', 'teamB': 'teamB'},
             {'teamA': 'teamC', 'teamB': 'teamD'}
         ]
+
+        dataLoader = NbaDataLoader()
+
         log('Starting call to user defined function')
-        result = main.predict(required_predictions, historical_data, current_season_matches, log)
+        result = main.predict(required_predictions, dataLoader, log)
         log('User defined function completed')
         log(f'User returned: {result}')
     except Exception as e:
--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
 # This is the main function you are requested to define.
 # required_predictions: a list of matches that you should predict. Each match is a dictionary
 #     containing two keys: "teamA", the name of the first team, and "teamB", the name of the second team.
-# historical_data: call to obtain the full data of all previous years. This data item does not change over time.
-# current_season_matches: call to obtain the full data of all previous games already played in the current season.
-#     This does not overlap with historical_data. This is empty when predicting the first match
-#     of the current season. As the season progresses, this item will gradually include
-#     previous matches of the current season.
+# data_loader: a NbaDataLoader class instance.
+#     Contains getSeason, getGame and getPlayers methods. Please refer to simulate.py for details about
+#     the provided methods.
 # log: you can call log('message') to log any information you want. These messages are automatically
 #     printed to console, and to the logfiles when running on the server side.
 #     All logs sent to this logger can be retrieved from our servers (up to a maximum size), so
@@ -14,10 +12,10 @@
 #     The difference between the scores: teamAscore - teamBscore
 #     And the sum of the scores: teamAscore + teamBscore
 #     The result format should be a list of {"teamA": "X", "teamB": "Y", "diff": 25, "sum": 163}
-def predict(required_predictions, historical_data, current_season_matches, log):
+def predict(required_predictions, data_loader, log):
     # This example code predicts that the first team always wins every match
-    hist = historical_data()
-    curr = current_season_matches()
+    games2010 = data_loader.getSeason('2010')
+    log(f'{games2010}')
     result = []
     for match in required_predictions:
         # In this example we always predict that teamA wins by 100 points
